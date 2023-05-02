f379e79a1d1a71c933e64e4ab0ca7a6b3864ea57
Cristian Reyes <reyes.cristian182@gmail.com>
2023-05-02 15:59:25 -0600

    Revert "Revert "Revert "Revert "added project files""""
    This reverts commit 10cc823560b705254a603df0fb549269da42cd9d.

---
+++ src_picoDeGallo_png.bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["src_picoDeGallo_png"],{
+
+/***/ "./src/picoDeGallo.png":
+/*!*****************************!*\
+  !*** ./src/picoDeGallo.png ***!
+  \*****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "772e0c4d6a1aed33d416.png";
+
+/***/ })
+
+}]);
---
+++ src_sodas_png.bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["src_sodas_png"],{
+
+/***/ "./src/sodas.png":
+/*!***********************!*\
+  !*** ./src/sodas.png ***!
+  \***********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "0712eb99547e36c223dd.png";
+
+/***/ })
+
+}]);
